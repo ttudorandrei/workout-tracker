da3028ad7e47c9c1f3d54477368f0f755b2582fd
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-12 13:01:14 +0100

    refactor folder structure

--- src/controllers/html/workouts.js
+++ src/controllers/html/workouts.js
@@ -2,21 +2,21 @@ const path = require("path");
 
 const renderHomepage = async (req, res) => {
   try {
-    res.sendFile(path.join(__dirname, "../../public/index.html"));
+    res.sendFile(path.join(__dirname, "../../../public/index.html"));
   } catch (error) {
     console.info(error.message);
   }
 };
 const renderStatsPage = async (req, res) => {
   try {
-    res.sendFile(path.join(__dirname, "../../public/stats.html"));
+    res.sendFile(path.join(__dirname, "../../../public/stats.html"));
   } catch (error) {
     console.info(error.message);
   }
 };
 const renderExercisePage = async (req, res) => {
   try {
-    res.sendFile(path.join(__dirname, "../../public/exercise.html"));
+    res.sendFile(path.join(__dirname, "../../../public/exercise.html"));
   } catch (error) {
     console.info(error.message);
   }
