7cd1829915ef833a39b03abc1bc0dfbcde7bd729
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-17 11:11:04 +0100

    add code to aggregate workouts

--- src/controllers/api/workouts.js
+++ src/controllers/api/workouts.js
@@ -2,7 +2,10 @@ const { Workout } = require("../../models");
 
 const getAllWorkouts = async (req, res) => {
   try {
-    const workouts = await Workout.find({});
+    const workouts = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ]);
+
     return res.status(200).json(workouts);
   } catch (error) {
     console.info(error.message);
@@ -42,8 +45,13 @@ const addWorkout = async (req, res) => {
 
 const getRange = async (req, res) => {
   try {
-    const workoutRange = Workout.aggregate();
-    return res.status(200).json(workoutRange);
+    const workoutDuration = await Workout.aggregate([
+      { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+    ])
+      .sort({ day: 1 })
+      .limit(7);
+
+    return res.status(200).json(workoutDuration);
   } catch (error) {
     console.info();
     return res.status(500).json({ error: "Failed to get workout" });
